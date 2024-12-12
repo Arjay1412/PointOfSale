957bc0b255e905e38c2ad09190d5ccec36e293ae
Arjay1412 <timbancayaarjay@gmail.com>
2024-12-12 23:01:57 +0800

    Database Connection Stablished

--- api.py
+++ api.py
@@ -4,6 +4,12 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+# Database Connection
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "admin"
+app.config["MYSQL_DB"] = "posdb"
+
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
 
@@ -13,6 +19,27 @@ mysql = MySQL(app)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/products", methods=["GET"])
+def get_products():
+    cur = mysql.connection.cursor()
+    query = """SELECT * FROM posdb.product"""
+    cur.execute(query)
+
+    data = cur.fetchall()
+    cur.close()
+
+    return data
+
+@app.route("/products", methods=["GET"])
+def get_products():
+    cur = mysql.connection.cursor()
+    query = """SELECT * FROM posdb.product"""
+    cur.execute(query)
+
+    data = cur.fetchall()
+    cur.close()
+
+    return data
 
 if __name__ == "__main__":
     app.run(debug=True)
